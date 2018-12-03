e8af6474df15910deb7a51d23c71c92f3b4f5db3
thaint <thaint@blueseed.tv>
2018-12-03 11:43:01 +0700

    Add organization service

---
+++ organization-service/src/main/java/com/organizationservice/client/EmployeeClient.java
@@ -0,0 +1,16 @@
+package com.organizationservice.client;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.organizationservice.model.Employee;
+
+@FeignClient(name = "employee-service")
+public interface EmployeeClient {
+
+    @GetMapping("/organization/{organizationId}")
+    List<Employee> findByOrganization(@PathVariable("organizationId") Long organizationId);
+}
