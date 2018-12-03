c7d953c1217fa20c292bdbf4ed4554d7e53ada29
thaint <thaint@blueseed.tv>
2018-12-03 10:55:21 +0700

    Add first microservice - Employee service

---
+++ employee-service/src/main/java/com/example/employeeservice/model/Employee.java
@@ -0,0 +1,124 @@
+package com.example.employeeservice.model;
+
+public class Employee {
+    private Long id;
+    private Long organizationId;
+    private Long departmentId;
+    private String name;
+    private int age;
+    private String position;
+
+    public Employee() {
+
+    }
+
+    public Employee(Long organizationId, Long departmentId, String name, int age, String position) {
+	this.organizationId = organizationId;
+	this.departmentId = departmentId;
+	this.name = name;
+	this.age = age;
+	this.position = position;
+    }
+
+    /**
+     * @return the id
+     */
+    public Long getId() {
+	return id;
+    }
+
+    /**
+     * @param id
+     *            the id to set
+     */
+    public void setId(Long id) {
+	this.id = id;
+    }
+
+    /**
+     * @return the organizationId
+     */
+    public Long getOrganizationId() {
+	return organizationId;
+    }
+
+    /**
+     * @param organizationId
+     *            the organizationId to set
+     */
+    public void setOrganizationId(Long organizationId) {
+	this.organizationId = organizationId;
+    }
+
+    /**
+     * @return the departmentId
+     */
+    public Long getDepartmentId() {
+	return departmentId;
+    }
+
+    /**
+     * @param departmentId
+     *            the departmentId to set
+     */
+    public void setDepartmentId(Long departmentId) {
+	this.departmentId = departmentId;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+	return name;
+    }
+
+    /**
+     * @param name
+     *            the name to set
+     */
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    /**
+     * @return the age
+     */
+    public int getAge() {
+	return age;
+    }
+
+    /**
+     * @param age
+     *            the age to set
+     */
+    public void setAge(int age) {
+	this.age = age;
+    }
+
+    /**
+     * @return the position
+     */
+    public String getPosition() {
+	return position;
+    }
+
+    /**
+     * @param position
+     *            the position to set
+     */
+    public void setPosition(String position) {
+	this.position = position;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+	return "Employee [id=" + id + ", organizationId=" + organizationId + ", departmentId=" + departmentId
+		+ ", name=" + name + ", age=" + age + ", position=" + position + "]";
+    }
+
+}
